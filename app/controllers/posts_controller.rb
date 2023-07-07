195871724f38346c69c240f580016e8e348af227
brendonjake <macapaz.b1998@gmail.com>
2023-07-07 14:39:19 +0800

    added create and edit to post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post].permit(:title, :content))
+    if @post.save
+      flash[:notice] = 'Post created successfully'
+      redirect_to posts_path
+    else
+      flash.now[:alert] = 'Post create failed'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+end
