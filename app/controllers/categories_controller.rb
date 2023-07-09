5c28678aadc67a51d30a3b9585452d2b36128828
brendonjake <macapaz.b1998@gmail.com>
2023-07-09 21:39:40 +0800

    Editing categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -31,8 +31,11 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
-    @category.destroy
-    flash[:notice] = 'Category deleted successfully'
+    if @category.destroy
+      flash[:notice] = 'Category deleted successfully'
+    else
+      flash[:alert] = 'Delete failed'
+    end
     redirect_to categories_path
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+categories = ['COVID', 'Earthquake', 'Requirements']
+
+categories.each do |category|
+  category = Category.find_or_create_by(name: category)
+end
+
 10.times do
   user = User.create!(email: Faker::Internet.email, password: 'qwer4321', password_confirmation: "qwer4321")
   puts "create user id: #{user.id}, email: #{user.email}"
@@ -12,17 +18,9 @@ end
 
 30.times do |i|
   puts "start create #{i} post"
-  post = Post.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, address: Faker::Lorem.sentence, user: User.all.sample)
-  (1..20).to_a.sample.times do
+  post = Post.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, user: User.all.sample, categories: Category.all.sample(1), address: Faker::Address.full_address)
     Comment.create(content: Faker::Lorem.sentence, user: User.all.sample, post: post)
-  end
   puts "finish #{i} post"
 end
 
-categories = ['COVID', 'Earthquake', 'Requirements']
 
-categories.each do |category|
-  category = Category.find_or_create_by(name: category)
-end
-
-Category.all.sample (10)
