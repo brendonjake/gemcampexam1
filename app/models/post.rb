0741de5888770b520bd70ef675e6e44fcde81da3
brendonjake <macapaz.b1998@gmail.com>
2023-07-09 16:46:54 +0800

    Installing Carrier Wave Gem

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   validates :address, presence: true
 
   belongs_to :user
+  mount_uploader :image, ImageUploader
   has_many :comments
   has_many :post_category_ships
   has_many :categories, through: :post_category_ships
