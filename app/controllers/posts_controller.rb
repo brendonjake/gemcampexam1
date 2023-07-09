139e49d2f8dc080087ac45158557793481b23667
brendonjake <macapaz.b1998@gmail.com>
2023-07-09 16:12:30 +0800

    installing kaminari gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :validate_post_owner, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.includes(:categories).all
+    @posts = Post.includes(:categories, :user).page(params[:page]).per(5)
   end
 
   def new
@@ -58,7 +58,7 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids: [])
+    params.require(:post).permit(:title, :content, :address, category_ids: [])
   end
 end
 
